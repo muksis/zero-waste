3831cc26e04e853690e6dbe27cd37b2866d4275d
muksis <60219890+muksis@users.noreply.github.com>
2022-07-14 14:17:27 -0400

    Add use to the database

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -5,6 +5,8 @@ import {
   createUserWithEmailAndPassword,
   updateProfile,
 } from 'firebase/auth';
+import { setDoc, doc, serverTIme, serverTimestamp } from 'firebase/firestore';
+import { db } from '../firebase.config';
 import { FaChevronRight } from 'react-icons/fa';
 import visibilityIcon from '../assets/svg/visibilityIcon.svg';
 
@@ -28,17 +30,28 @@ function SignUp() {
 
   const onSubmit = async (e) => {
     e.preventDefault();
+
     try {
       const auth = getAuth();
+
       const userCredential = await createUserWithEmailAndPassword(
         auth,
         email,
         password
       );
+
       const user = userCredential.user;
+
       updateProfile(auth.currentUser, {
         displayName: name,
       });
+
+      const formDataCopy = { ...formData };
+      delete formDataCopy.password;
+      formDataCopy.timestamp = serverTimestamp();
+
+      await setDoc(doc(db, 'users', user.uid), formDataCopy);
+
       navigate('/');
     } catch (error) {
       console.log(error);
