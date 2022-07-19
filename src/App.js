fc65bba343cb700667ea1ae0b5f804d196de1d3e
muksis <60219890+muksis@users.noreply.github.com>
2022-07-19 16:56:23 -0400

    Add home page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar';
 import PrivateRoute from './components/PrivateRoute';
 import Home from './pages/Home';
 import Free from './pages/Free';
+import Category from './pages/Category';
 import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
@@ -17,6 +18,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/free' element={<Free />} />
+          <Route path='/category/:categoryName' element={<Category />} />
           <Route path='/profile' element={<PrivateRoute />}>
             <Route path='/profile' element={<Profile />} />
           </Route>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,41 @@
+import { Link } from 'react-router-dom';
 import together from '../assets/png/together.png';
+import rentCategoryImage from '../assets/jpg/rentCategoryImage.jpg';
+import sellCategoryImage from '../assets/jpg/sellCategoryImage.jpg';
 
 function Home() {
   return (
-    <div>
-      <h1>
-        <img src={together} alt='together' width='24px' height='24px' />
-        Home
-      </h1>
+    <div className='home'>
+      <header>
+        <p className='pageHeader'>
+          <img src={together} alt='together' width='24px' height='24px' />
+          Home
+        </p>
+      </header>
+
+      <main>
+        {/* Slider */}
+
+        <p className='homeCategoryHeading'>Categories</p>
+        <div className='homeCategories'>
+          <Link to='/category/rent'>
+            <img
+              src={rentCategoryImage}
+              alt='rent'
+              className='homeCategoryImg'
+            />
+            <p className='homeCategory'>Things for rent</p>
+          </Link>
+          <Link to='/category/sell'>
+            <img
+              src={sellCategoryImage}
+              alt='sell'
+              className='homeCategoryImg'
+            />
+            <p className='homeCategory'>Things for sale</p>
+          </Link>
+        </div>
+      </main>
     </div>
   );
 }
