4948025a8232a0939cf925058ebca9d3afb8638b
muksis <60219890+muksis@users.noreply.github.com>
2022-07-21 17:06:51 -0400

    Add create listing form

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -10,12 +10,23 @@ const CreateListing = () => {
     type: 'rent',
     name: '',
     address: '',
-    free: false,
     price: 0,
+    description: '',
     images: {},
     latitude: 0,
     longitude: 0,
   });
+
+  const {
+    type,
+    name,
+    address,
+    price,
+    description,
+    images,
+    latitude,
+    longitude,
+  } = formData;
 
   const auth = getAuth();
   const navigate = useNavigate();
@@ -38,11 +49,197 @@ const CreateListing = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isMounted]);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleMutate = (e) => {
+    let boolean = null;
+
+    if (e.target.value === 'true') {
+      boolean = true;
+    }
+
+    if (e.target.value === 'false') {
+      boolean = false;
+    }
+
+    // Files
+    if (e.target.files) {
+      setFormData((prevState) => ({
+        ...prevState,
+        images: e.target.files,
+      }));
+    }
+
+    //Text/Booleans/Numbers
+    if (!e.target.files) {
+      setFormData((prevState) => ({
+        ...prevState,
+        [e.target.id]: boolean ?? e.target.value,
+      }));
+    }
+  };
+
   if (loading) {
     return <Spinner />;
   }
 
-  return <div>createListing</div>;
+  return (
+    <div className='profile'>
+      <header>
+        <p className='pageHeader'>Create a Listing</p>
+      </header>
+
+      <main>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor='type' className='formLabel'>
+            Sell / Let / Give Away
+          </label>
+          <div className='formButtons'>
+            <button
+              type='button'
+              className={type === 'sale' ? 'formButtonActive' : 'formButton'}
+              id='type'
+              value='sale'
+              onClick={handleMutate}
+            >
+              Sell
+            </button>
+            <button
+              type='button'
+              className={type === 'rent' ? 'formButtonActive' : 'formButton'}
+              id='type'
+              value='rent'
+              onClick={handleMutate}
+            >
+              Let
+            </button>
+            <button
+              type='button'
+              className={type === 'free' ? 'formButtonActive' : 'formButton'}
+              id='type'
+              value='free'
+              onClick={handleMutate}
+            >
+              Give Away
+            </button>
+          </div>
+
+          <label htmlFor='name' className='formLabel'>
+            Name
+          </label>
+          <input
+            className='formInputName'
+            type='text'
+            id='name'
+            value={name}
+            onChange={handleMutate}
+            maxLength='32'
+            minLength='10'
+            required
+          />
+
+          <label htmlFor='address' className='formLabel'>
+            Address
+          </label>
+          <textarea
+            className='formInputAddress'
+            type='text'
+            id='address'
+            value={address}
+            onChange={handleMutate}
+            required
+          />
+
+          {!geolocationEnabled && (
+            <div className='formLatLng flex'>
+              <div>
+                <label htmlFor='latitude' className='formLabel'>
+                  Latitude
+                </label>
+                <input
+                  className='formInputSmall'
+                  type='number'
+                  id='latitude'
+                  value={latitude}
+                  onChange={handleMutate}
+                  required
+                />
+              </div>
+              <div>
+                <label htmlFor='longitude' className='formLabel'>
+                  Longitude
+                </label>
+                <input
+                  className='formInputSmall'
+                  type='number'
+                  id='longitude'
+                  value={longitude}
+                  onChange={handleMutate}
+                  required
+                />
+              </div>
+            </div>
+          )}
+
+          {type !== 'free' && (
+            <>
+              <label htmlFor='price' className='formLabel'>
+                Price
+              </label>
+              <div className='formPriceDiv'>
+                <input
+                  className='formInputSmall'
+                  type='number'
+                  id='price'
+                  value={price}
+                  onChange={handleMutate}
+                  min='50'
+                  max='750000000'
+                  required
+                />
+                {type === 'rent' && <p className='formPriceText'>$ / month</p>}
+              </div>
+            </>
+          )}
+
+          <label htmlFor='description' className='formLabel'>
+            Description
+          </label>
+          <textarea
+            className='formInputAddress'
+            type='text'
+            id='description'
+            value={description}
+            onChange={handleMutate}
+            required
+          />
+
+          <label htmlFor='images' className='formLabel'>
+            Images
+          </label>
+          <p className='imagesInfo'>
+            The first image will be the cover (max 6).
+          </p>
+          <input
+            className='formInputFile'
+            type='file'
+            id='images'
+            onChange={handleMutate}
+            max='6'
+            accept='.jpg, .png, .jpeg'
+            multiple
+            required
+          />
+
+          <button className='primaryButton createListingButton' type='submit'>
+            Create Listing
+          </button>
+        </form>
+      </main>
+    </div>
+  );
 };
 
 export default CreateListing;
